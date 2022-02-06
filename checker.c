1686184727003140d615adbfc74135f51683c507
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-02-06 20:20:41 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -5,7 +5,6 @@ int IsTemperatureOK(float temperature){
   int TemperatureOK;
   if(temperature < 0 || temperature > 45){
     TemperatureOK = 0;
-    print(&FindTempertaureHIGHORLOW);
   }
   else{
      TemperatureOK = 1;
@@ -18,7 +17,6 @@ int IsSocOK(float soc){
   int SocOK;
   if(soc < 20 || soc > 80){
     SocOK = 0;
-    print(&FindSocHIGHORLOW);
   }
   else{
     SocOK = 1;
@@ -26,7 +24,7 @@ int IsSocOK(float soc){
  }
 
 int IsChargRateOK(float chargeRate){
-  return(chargeRate >= 0.8)
+  return(chargeRate >= 0.8);
  }
 
 char* FindTempertaureHIGHORLOW(float temperature){
@@ -51,11 +49,11 @@ char* FindSocHIGHORLOW(float soc){
   return SocHIGHORLOW;
 }
 
-int batteryIsOk((int (*fpIsTemperatureOK)(float),int (*fpIsSocOK)(float),int (*fpIsChargRateOK)(float)){
-  int IsBatteryOK;
-  int TempStatus = fpIsTemperatureOK(float);
-  int SocStatus  =  fpIsSocOK(float);
-  int ChargeRateStatus =  fpIsChargRateOK(float);
+int batteryIsOk(float tempertaure, float soc, float chargerate, int (*fpIsTemperatureOK)(float),int (*fpIsSocOK)(float),int (*fpIsChargRateOK)(float)){
+
+  int TempStatus = fpIsTemperatureOK(temperature);
+  int SocStatus  =  fpIsSocOK(soc);
+  int ChargeRateStatus =  fpIsChargRateOK(chargerate);
   return IsBatteryOK = (TempStatus && SocStatus && ChargeRateStatus);
 }
 
