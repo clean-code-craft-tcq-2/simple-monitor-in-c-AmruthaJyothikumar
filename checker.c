101676993a192f305767be5f9fa8f32c2ad7fb54
AmruthaJyothikumar <90920630+AmruthaJyothikumar@users.noreply.github.com>
2022-02-07 21:44:44 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -61,6 +61,6 @@ void print(char *(fpdisplaystring)(float)){
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7, 1,1,1);
-  assert(!batteryIsOk(50, 85, 0,0,0,0);
+  assert(batteryIsOk(25, 70, 0.7, 1,1,1));
+  assert(!batteryIsOk(50, 85, 0,0,0,0));
 }
